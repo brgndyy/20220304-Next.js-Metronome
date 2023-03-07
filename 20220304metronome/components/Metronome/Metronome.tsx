1a948f7845bb235c482ade45c438508fd486b93b
brgndy <brgndy96@gmail.com>
2023-03-07 16:58:52 +0900

    msg

--- 20220304metronome/components/Metronome/Metronome.tsx
+++ 20220304metronome/components/Metronome/Metronome.tsx
@@ -1,10 +1,26 @@
 "use client";
-import React, { useRef, useState } from "react";
-import Sound from "components/Sound/Sound";
+import React, { useRef, useState, useEffect } from "react";
+
+import tickSound from "../../public/sounds/tick1.wav";
+import tockSound from "../../public/sounds/tick2.wav";
 
 export default function Metronome() {
   const [bpm, setBpm] = useState(60);
+  const [playing, setPlaying] = useState(false);
   const inputRef = useRef(null);
+  const [tick, setTick] = useState<HTMLAudioElement>();
+  const [tock, setTock] = useState<HTMLAudioElement>();
+
+  // 초기 렌더링 당시에 메트로놈 사운드 설정해주기.
+
+  useEffect(() => {
+    setTick(new Audio(tickSound));
+    setTock(new Audio(tockSound));
+  }, []);
+
+  const metronomeSoundHandler = () => {
+    tick && tick.play();
+  };
 
   const metronomeInputBlurHandler = () => {
     // 메트로놈 최소값이 1보다 작으면 1로 설정
@@ -18,6 +34,16 @@ export default function Metronome() {
       setBpm(1);
     }
   };
+
+  // bpm 소리가 반복되도록 하기
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      metronomeSoundHandler();
+    }, (60 / bpm) * 1000);
+
+    return () => clearInterval(interval);
+  }, [bpm]);
 
   const metronomeLoudnessHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const num = parseInt(e.target.value);
@@ -37,7 +63,7 @@ export default function Metronome() {
         onBlur={metronomeInputBlurHandler}
       />
       <p>현재 bpm : {Number.isNaN(bpm) ? "" : bpm}</p>
-      <Sound bpm={bpm} />
+      <button onClick={metronomeSoundHandler}>Play</button>
     </>
   );
 }
